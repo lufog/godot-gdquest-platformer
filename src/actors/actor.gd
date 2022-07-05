025325d8879ddc806e8ad811aa0442556d51f91c
Aleksey Smirnov <debugger94@gmail.com>
2022-07-05 21:49:35 +0300

    Chapter 1 player and enemy

---
+++ src/actors/actor.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+class_name Actor
+
+
+@export var speed := Vector2(400.0, 400.0)
+
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _ready() -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	velocity.y = min(velocity.y, speed.y)
+	velocity.x = min(velocity.x, speed.x)
+	
+	move_and_slide()
---
+++ src/actors/player.gd
@@ -0,0 +1,44 @@
+extends Actor
+
+
+
+func _physics_process(delta: float) -> void:
+	var input_direction := Input.get_axis("move_left", "move_right")
+	
+	if input_direction != 0:
+		velocity.x = speed.x * input_direction
+	else:
+		velocity.x = 0
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -speed.y
+	
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y = 0
+	
+	super._physics_process(delta)
+
+
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+#
+## Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
+#
+#
+#
+#func _physics_process(delta: float) -> void:
+#	
+#
+#	# Handle Jump.
+#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+#		velocity.y = JUMP_VELOCITY
+#
+#	# Get the input direction and handle the movement/deceleration.
+#	# As good practice, you should replace UI actions with custom gameplay actions.
+#	var direction := Input.get_axis("ui_left", "ui_right")
+#	if direction:
+#		velocity.x = direction * SPEED
+#	else:
+#		velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+#	move_and_slide()
